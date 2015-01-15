a065b75e6fbb20028fa677f061479560a75b89e7
Jonathan Reem <jonathan.reem@gmail.com>
2015-01-15 03:17:25 +0100

    (feat) Implement the existing RcStringExt methods.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 
 use std::rc::Rc;
 use std::sync::Arc;
+use std::mem;
 
 pub trait IntoRc {
     type Rc;
@@ -40,11 +41,85 @@ pub trait RcStrExt {
 impl RcStringExt for Rc<String> {
     type RcSlice = RcStr;
 
+    fn as_rc_slice(&self) -> RcStr {
+        RcStr {
+            data: &self[] as *const _,
+            rc: self.clone()
+        }
+    }
+
+    fn rc_slice_to(&self, to: usize) -> RcStr {
+        RcStr {
+            data: &self[..to] as *const _,
+            rc: self.clone()
+        }
+    }
+
+    fn rc_slice_from(&self, from: usize) -> RcStr {
+        RcStr {
+            data: &self[from..] as *const _,
+            rc: self.clone()
+        }
+    }
+
+    fn rc_slice(&self, from: usize, to: usize) -> RcStr {
+        RcStr {
+            data: &self[from..to] as *const _,
+            rc: self.clone()
+        }
+    }
+
+    fn rc_split_at(&self, at: usize) -> (RcStr, RcStr) {
+        (RcStr {
+            data: &self[..to] as *const _,
+            rc: self.clone()
+        }, RcStr {
+            data: &self[to..] as *const _,
+            rc: self.clone()
+        })
+    }
 }
 
-impl RcString for Arc<String> {
+impl RcStringExt for Arc<String> {
     type RcSlice = ArcStr;
 
+    fn as_rc_slice(&self) -> ArcStr {
+        ArcStr {
+            data: &self[] as *const _,
+            arc: self.clone()
+        }
+    }
+
+    fn rc_slice_to(&self, to: usize) -> ArcStr {
+        ArcStr {
+            data: &self[..to] as *const _,
+            arc: self.clone()
+        }
+    }
+
+    fn rc_slice_from(&self, from: usize) -> ArcStr {
+        ArcStr {
+            data: &self[from..] as *const _,
+            arc: self.clone()
+        }
+    }
+
+    fn rc_slice(&self, from: usize, to: usize) -> ArcStr {
+        ArcStr {
+            data: &self[from..to] as *const _,
+            arc: self.clone()
+        }
+    }
+
+    fn rc_split_at(&self, at: usize) -> (ArcStr, ArcStr) {
+        (ArcStr {
+            data: &self[..to] as *const _,
+            arc: self.clone()
+        }, ArcStr {
+            data: &self[to..] as *const _,
+            arc: self.clone()
+        })
+    }
 }
 
 impl RcStrExt for RcStr {
@@ -59,6 +134,7 @@ impl RcStrExt for ArcStr {
 
 impl IntoRc for String {
     type Rc = Rc<String>;
+    type Arc = Arc<String>;
 
     fn rc(self) -> Rc<String> { Rc::new(self) }
     fn arc(self) -> Arc<String> { Arc::new(self) }
@@ -66,13 +142,33 @@ impl IntoRc for String {
 
 #[derive(Clone)]
 pub struct RcStr {
-    data: &'static str,
+    data: *const str,
+    // Unless the internals of Rc are exposed, we can't do better than this.
+    //
+    // In an ideal world we would store a `*mut Cell<usize>` which points to
+    // the strong count of the underlying `Rc<String>`, eliminating an
+    // unnecessary layer of indirection.
     rc: Rc<String>
+}
+
+impl Deref for RcStr {
+    type Target = str;
+
+    fn deref(&self) -> &str { self.data }
 }
 
 #[derive(Clone)]
 pub struct ArcStr {
-    data: &'static str,
+    data: *const str,
+
+    // Ditto from the RcStr comment, except we'd store a reference to the
+    // AtomicUsize.
     arc: Arc<String>
+}
+
+impl Deref for ArcStr {
+    type Target = str;
+
+    fn deref(&self) -> &str { self.data }
 }
 
